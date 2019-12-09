3e1144db1810d23f7aadf4312bd31def0daae197
Marek Kobyliński <Marecki1121@gmail.com>
2019-12-09 21:19:32 +0100

    add: correct showing target value in screen && screen_top

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const screen = document.querySelector('.screen');
 const screen_top = document.querySelector('.screen_top');
 const buttons = document.querySelectorAll('.calc-button');
-const length = document.querySelector('.screen').innerHTML.length;
-
+const symbols = ["÷", "+","×","–"];
+const numbers = ["0","1","2","3","4","5","6","7","8","9"]
 buttons.forEach(div => {
     div.addEventListener('click', e => {
+        const length = document.querySelector('.screen').innerHTML.length;
         if (screen.innerHTML === "0") {
             screen.innerHTML = ""
         }
         switch (e.target.innerHTML) {
             case 'C':
                 screen.innerHTML = "0";
+                screen_top.innerHTML = "0"
                 break;
             case '←': 
             screen.innerHTML = screen.innerHTML.slice(0, -1);
@@ -31,23 +33,25 @@ buttons.forEach(div => {
                 screen.innerHTML += e.target.innerHTML;
                 break;
             case '+':
-                if (screen.innerHTML.slice(document.querySelector('.screen').innerHTML.length-1) === "+" || "÷") {
-                    console.log("true")
-                    break;
-                } else {
-                    console.log("false")
-                    screen.innerHTML += "+";
-                break;
-                };
             case '÷':
-                if (screen.innerHTML.slice(document.querySelector('.screen').innerHTML.length-1) === "÷" || "+") {
-                    console.log("true")
+            case '–':
+            case '×':
+                if (symbols.includes(screen.innerHTML.slice(length-1)) && !(screen.innerHTML === "0")) {
+                        break;
+                    } else {
+                        if(screen.innerHTML.length >= 1) {
+                            if(screen_top.innerHTML === "0"){
+                                screen_top.innerHTML = ""
+                            }
+                            console.log("true")
+                            screen.innerHTML += e.target.innerHTML;
+                            screen_top.innerHTML += screen.innerHTML;
+                            screen.innerHTML = "0";
+                        } else { console.log("false")
+                        screen.innerHTML += e.target.innerHTML;
                     break;
-                } else {
-                    console.log("false")
-                    screen.innerHTML += "÷";
-                break;
-                };
+                        }
+                    }
         }   
         console.log(e.target.innerHTML)
         console.log(length);
