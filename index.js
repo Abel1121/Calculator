57c9cfe2eb3a8658e8090280e2e67bea7fd1fc10
Marek Kobyliński <Marecki1121@gmail.com>
2019-12-10 00:44:34 +0100

    ITS WORKING !! 1.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const screen = document.querySelector('.screen');
 const screen_top = document.querySelector('.screen_top');
 const buttons = document.querySelectorAll('.calc-button');
 const symbols = ["÷", "+","×","–"];
-const numbers = ["0","1","2","3","4","5","6","7","8","9"]
+let value = [];
 buttons.forEach(div => {
     div.addEventListener('click', e => {
         const length = document.querySelector('.screen').innerHTML.length;
@@ -13,6 +13,7 @@ buttons.forEach(div => {
             case 'C':
                 screen.innerHTML = "0";
                 screen_top.innerHTML = "0"
+                value = [];
                 break;
             case '←': 
             screen.innerHTML = screen.innerHTML.slice(0, -1);
@@ -30,13 +31,14 @@ buttons.forEach(div => {
             case '8':
             case '9':
             case '0':
+                value.push(e.target.innerHTML)
                 screen.innerHTML += e.target.innerHTML;
                 break;
             case '+':
             case '÷':
             case '–':
             case '×':
-                if (symbols.includes(screen.innerHTML.slice(length-1)) && !(screen.innerHTML === "0")) {
+                if (symbols.includes(screen.innerHTML.slice(-1)) && !(screen.innerHTML === "0")) {
                         break;
                     } else {
                         if(screen.innerHTML.length >= 1) {
@@ -44,16 +46,41 @@ buttons.forEach(div => {
                                 screen_top.innerHTML = ""
                             }
                             console.log("true")
+                            if(e.target.innerHTML === "÷"){
+                                value.push("÷") 
+                            } else if (e.target.innerHTML === "×"){
+                                value.push("×")
+                            } else if (e.target.innerHTML === "–"){
+                                value.push("-")
+                            } 
+                            else {
+                                value.push(e.target.innerHTML)
+                            }
                             screen.innerHTML += e.target.innerHTML;
                             screen_top.innerHTML += screen.innerHTML;
                             screen.innerHTML = "0";
-                        } else { console.log("false")
-                        screen.innerHTML += e.target.innerHTML;
-                    break;
                         }
+                    };
+                    break;
+            case "=":
+                if (symbols.includes(screen.innerHTML.slice(length-1)) && !(screen.innerHTML === "0")) {
+                    break;
+                } else {
+                    for(let i=0; i <= value.length; i++){
+                        if(value[i] === "÷" ) {value[i] = "/"} else if (value[i] === "×") {value[i] = "*"}
                     }
+                    value.push(screen.innerHTML)
+                    value.splice(-1,1)
+                        screen_top.innerHTML += screen.innerHTML + "=";
+                        const str = value.join('')
+                        screen.innerHTML = eval(str)
+                        console.log(str)
+                        console.log(eval(str))
+                    }
+                    break;
         }   
         console.log(e.target.innerHTML)
         console.log(length);
+        console.log(value);
     })
 });
